f2ccedaf604d31a23d3e6bf98872a2c431c9e769
Dimas Angkasa Nurindra <mas.angkasa27@gmail.com>
2025-08-18 14:05:51 +0700

    Refactor ModeToggle component to use DropdownMenu for improved theme selection UI

--- src/components/fragments/mode-toggle.tsx
+++ src/components/fragments/mode-toggle.tsx
@@ -3,14 +3,38 @@
 import * as React from "react";
 import { useTheme } from "next-themes";
 
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LuMoon, LuSun } from "react-icons/lu";
+
 export function ModeToggle() {
   const { setTheme } = useTheme();
 
   return (
-    <div>
-      <button onClick={() => setTheme("light")}>Light</button>
-      <button onClick={() => setTheme("dark")}>Dark</button>
-      <button onClick={() => setTheme("system")}>System</button>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <LuSun className="size-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <LuMoon className="absolute size-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
