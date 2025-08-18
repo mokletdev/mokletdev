073e02df943dd9c9809b1f1387386cb16e8486e6
Dimas Angkasa Nurindra <mas.angkasa27@gmail.com>
2025-08-18 14:06:24 +0700

    Refactor HomeContainer to replace ModeToggle with Navbar and enhance HeroSection with Button component for improved UI interaction

--- src/containers/home/home.tsx
+++ src/containers/home/home.tsx
@@ -1,9 +1,9 @@
-import { ModeToggle } from "@/components/fragments/mode-toggle";
 import { GitHubRepository, GitHubUserLite } from "@/types/github";
 import { ReposSection } from "./section/repos";
 import { HeroSection } from "./section/hero";
 import { ContributeSection } from "./section/contribute";
 import { Footer } from "@/components/fragments/footer";
+import { Navbar } from "@/components/fragments/navbar";
 
 export const HomeContainer = ({
   repos,
@@ -14,7 +14,7 @@ export const HomeContainer = ({
 }) => {
   return (
     <main className="mx-auto w-[calc(100%-2rem)] sm:w-[calc(100%-4rem)] max-w-7xl">
-      <ModeToggle />
+      <Navbar />
       <HeroSection />
       <ReposSection repos={repos} />
       <ContributeSection members={members} />
--- src/containers/home/section/hero/hero.tsx
+++ src/containers/home/section/hero/hero.tsx
@@ -1,6 +1,9 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { motion, useScroll, useTransform } from "motion/react";
+import Link from "next/link";
 import { useRef } from "react";
+import { LuArrowUpRight, LuGlobe } from "react-icons/lu";
 
 export const HeroSection = () => {
   const projectRef = useRef<HTMLElement | null>(null);
@@ -11,20 +14,24 @@ export const HeroSection = () => {
     offset: ["start start", "end start"],
   });
 
-  const scale = useTransform(scrollYProgress, [0, 0.8], [1, 0.6]);
+  const scale = useTransform(scrollYProgress, [0, 0.5], [1, 0.6]);
 
-  const rotate = useTransform(scrollYProgress, [0, 1], [-7, -21]);
+  const rotate = useTransform(scrollYProgress, [0, 0.5], [-7, -21]);
 
-  // const translateY = useTransform(scrollYProgress, [0, 1], [0, 1000]);
+  const translateY = useTransform(scrollYProgress, [0, 1], [0, 500]);
   const opacity = useTransform(scrollYProgress, [0.2, 0.5], [1, 0]);
 
   return (
     <section
       id="hero"
-      className="@container/hero h-svh w-full flex items-center justify-center relative overflow-hidden"
+      className="@container/hero h-screen w-full flex flex-col gap-32 items-center justify-center relative overflow-hidden"
       ref={projectRef}
     >
-      <motion.div style={{ scale, rotate, opacity }} className="transform">
+      <div />
+      <motion.div
+        style={{ scale, rotate, opacity, translateY }}
+        className="transform relative z-0"
+      >
         <h1 className="grid grid-cols-3 grid-rows-3 leading-none text-center font-medium font-rubik text-[25vw] sm:text-[20vw] md:text-[15vw] lg:text-[10vw] *:odd:text-red-500 *:even:text-amber-500">
           <motion.span
             initial={{
@@ -99,6 +106,28 @@ export const HeroSection = () => {
           </motion.span>
         </h1>
       </motion.div>
+      <div className="relative z-10 flex flex-col items-center gap-4">
+        <Button
+          variant="outline"
+          className="font-mono group hover:pr-8! focus:pr-8! relative duration-500 overflow-hidden"
+          asChild
+        >
+          <Link href="https://github.com/mokletdev" target="_blank">
+            <LuGlobe className="group-hover:-rotate-30 group-focus:-rotate-30 group-hover:text-accent-foreground group-focus:text-accent-foreground transition-all duration-300" />
+            <span>
+              <span className="max-sm:hidden">https://</span>
+              github.com/mokletdev
+            </span>
+            <LuArrowUpRight className="absolute right-3 group-hover:translate-y-0 translate-y-1/2 group-focus:opacity-100 group-hover:opacity-100 opacity-0 transition-all duration-500" />
+          </Link>
+        </Button>
+        {/* <p className="text-center text-muted-foreground max-w-2xl">
+          MokletDev is a student developer community under METIC (Moklet
+          Education of Technology and Informatic Club). We turn ideas into
+          working products, web apps, tools, and services used by our school and
+          external partners.
+        </p> */}
+      </div>
     </section>
   );
 };
